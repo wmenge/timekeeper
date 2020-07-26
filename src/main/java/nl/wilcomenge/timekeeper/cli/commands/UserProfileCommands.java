40651ee23db9e3b980683f380f0a7d5fc646c963
wilco menge <wilcomenge@gmail.com>
2020-07-26 11:38:43 +0200

    add hourly rate to profile

--- src/main/java/nl/wilcomenge/timekeeper/cli/commands/UserProfileCommands.java
+++ src/main/java/nl/wilcomenge/timekeeper/cli/commands/UserProfileCommands.java
@@ -5,11 +5,14 @@ import nl.wilcomenge.timekeeper.cli.service.UserProfileService;
 import nl.wilcomenge.timekeeper.cli.ui.view.ResultView;
 import org.jline.utils.AttributedString;
 import org.springframework.lang.NonNull;
+import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.Currency;
 
 @ShellComponent
 public class UserProfileCommands {
@@ -36,5 +39,53 @@ public class UserProfileCommands {
                     String.format("Fulltime percentage set to %.2f %%", profile.getFulltimeFactor().doubleValue()
                             * 100)).render();
         }
+    }
+
+    @ShellMethod("Set Currency")
+    public AttributedString setCurrency(@NonNull String currencyString) {
+        UserProfile profile = userProfileService.getProfile();
+        profile.setCurrency(Currency.getInstance(currencyString));
+        userProfileService.save(profile);
+        return showCurrency();
+    }
+
+    @ShellMethod("Show Currencuy")
+    public AttributedString showCurrency() {
+        UserProfile profile = userProfileService.getProfile();
+        if (profile.getCurrency() == null) {
+            return ResultView.build(ResultView.MessageType.WARNING, "Currency not set").render();
+        } else {
+            return ResultView.build(
+                    ResultView.MessageType.INFO,
+                    String.format("Currency set to %s", profile.getCurrency().getSymbol())).render();
+        }
+    }
+
+    @ShellMethod("Set Hourly rate")
+    @ShellMethodAvailability("rateAvailabilityCheck")
+    public AttributedString setHourlyRate(@NonNull String rateString) {
+        UserProfile profile = userProfileService.getProfile();
+        profile.setHourlyRate(BigDecimal.valueOf(Double.parseDouble(rateString)));
+        userProfileService.save(profile);
+        return showHourlyRate();
+    }
+
+    @ShellMethod("Show Hourly rate")
+    @ShellMethodAvailability("rateAvailabilityCheck")
+    public AttributedString showHourlyRate() {
+        UserProfile profile = userProfileService.getProfile();
+        if (profile.getHourlyRate() == null) {
+            return ResultView.build(ResultView.MessageType.WARNING, "Hourly rate not set").render();
+        } else {
+            return ResultView.build(
+                    ResultView.MessageType.INFO,
+                    String.format("Hourly rate set to %s %.2f", profile.getCurrency().getSymbol(), profile.getHourlyRate().doubleValue())).render();
+        }
+    }
+
+    public Availability rateAvailabilityCheck() {
+        return userProfileService.getProfile().getCurrency() != null
+                ? Availability.available()
+                : Availability.unavailable("no currency has been set");
     }
 }
--- src/main/java/nl/wilcomenge/timekeeper/cli/model/UserProfile.java
+++ src/main/java/nl/wilcomenge/timekeeper/cli/model/UserProfile.java
@@ -8,10 +8,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Currency;
 
 @Entity
 @Data
 public class UserProfile {
+
+    public static final String EUR_CURRENCY_CODE = "EUR";
 
     public UserProfile() {
 
@@ -23,5 +26,9 @@ public class UserProfile {
 
     @NonNull
     private BigDecimal fulltimeFactor;
+
+    private Currency currency = Currency.getInstance(EUR_CURRENCY_CODE);
+
+    private BigDecimal hourlyRate;
 
 }
